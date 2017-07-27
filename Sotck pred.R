255e4bc4e9580539c57a6288001dd59dace245bb
Andrew Yuen <andrewyuenck@gmail.com>
2017-07-27 12:58:06 +0800

    Update and rename Chapter3.R to Sotck pred.R

--- Sotck pred.R
+++ Sotck pred.R
@@ -5,104 +5,10 @@ library(DMwR)
 data(GSPC)
 
 
-
-###################################################
-### Handling time dependent data in R
-###################################################
-library(xts)
-x1 <- xts(rnorm(100),seq(as.POSIXct("2000-01-01"),len=100,by="day"))
-x1[1:5]
-x2 <- xts(rnorm(100),seq(as.POSIXct("2000-01-01 13:00"),len=100,by="min"))
-x2[1:4]
-x3 <- xts(rnorm(3),as.Date(c('2005-01-01','2005-01-10','2005-01-12')))
-x3
-
-
-x1[as.POSIXct("2000-01-04")]
-x1["2000-01-05"]
-x1["20000105"]
-x1["2000-04"]
-x1["2000-03-27/"]
-x1["2000-02-26/2000-03-03"]
-x1["/20000103"]
-
-
-mts.vals <- matrix(round(rnorm(25),2),5,5)
-colnames(mts.vals) <- paste('ts',1:5,sep='')
-mts <- xts(mts.vals,as.POSIXct(c('2003-01-01','2003-01-04',
-                    '2003-01-05','2003-01-06','2003-02-16')))
-mts
-mts["2003-01",c("ts2","ts5")]
-index(mts)
-coredata(mts)
-
-
 ###################################################
 ### Reading the data from the CSV file
 ###################################################
 GSPC <- as.xts(read.zoo('sp500.csv',header=T))
-
-
-###################################################
-### Getting the data from the Web
-###################################################
-library(tseries)
-GSPC <- as.xts(get.hist.quote("^GSPC",start="1970-01-02",
-          quote=c("Open", "High", "Low", "Close","Volume","AdjClose")))
-head(GSPC)
-GSPC <- as.xts(get.hist.quote("^GSPC",
-          start="1970-01-02",end='2016-09-15',
-          quote=c("Open", "High", "Low", "Close","Volume","AdjClose")))
-
-
-
-library(quantmod)
-getSymbols('0001.HK')
-
-
-
-getSymbols('^GSPC',from='1970-01-01',to='2016-09-15')
-colnames(GSPC) <- c("Open", "High", "Low", "Close","Volume","AdjClose")
-
-
-setSymbolLookup(IBM=list(name='IBM',src='yahoo'),
-                USDEUR=list(name='USD/EUR',src='oanda',
-                            from=as.Date('2009-01-01')))
-getSymbols(c('IBM','USDEUR'))
-head(IBM)
-head(USDEUR)
-
-
-
-###################################################
-### Reading the data from a MySQL database
-###################################################
-library(RODBC)
-ch <- odbcConnect("QuotesDSN",uid="myusername",pwd="mypassword")
-allQuotes <- sqlFetch(ch,"gspc")
-GSPC <- xts(allQuotes[,-1],order.by=as.Date(allQuotes[,1]))
-head(GSPC)
-odbcClose(ch)
-
-
-
-library(DBI)
-library(RMySQL)
-drv <- dbDriver("MySQL")
-ch <- dbConnect(drv,dbname="Quotes","myusername","mypassword")
-allQuotes <- dbGetQuery(ch,"select * from gspc")
-GSPC <- xts(allQuotes[,-1],order.by=as.Date(allQuotes[,1]))
-head(GSPC)
-dbDisconnect(ch)
-dbUnloadDriver(drv)
-
-
-setSymbolLookup(GSPC=list(name='gspc',src='mysql',
-         db.fields=c('Index','Open','High','Low','Close','Volume','AdjClose'),
-         user='xpto',password='ypto',dbname='Quotes'))
-getSymbols('GSPC')
-
-
                 
 ###################################################
 ### Defining the Prediction Tasks
@@ -127,8 +33,6 @@ addAvgPrice <- newTA(FUN=avgPrice,col=1,legend='AvgPrice')
 addT.ind <- newTA(FUN=T.ind,col='red',legend='tgtRet')
 addAvgPrice(on=1)
 addT.ind()
-
-
 
 myATR <- function(x) ATR(HLC(x))[,'atr']
 mySMI <- function(x) SMI(HLC(x))[,'SMI']
@@ -160,8 +64,6 @@ rf <- buildModel(data.model,method='randomForest',
 ex.model <- specifyModel(T.ind(IBM) ~ Delt(Cl(IBM),k=1:3))
 data <- modelData(ex.model,data.window=c('2009-01-01','2009-08-10'))
 
-
-
 varImpPlot(rf@fitted.model,type=1)
 
 
@@ -176,7 +78,6 @@ Tdata.train <- as.data.frame(modelData(data.model,
 Tdata.eval <- na.omit(as.data.frame(modelData(data.model,
                        data.window=c('2000-01-01','2009-09-15'))))
 Tform <- as.formula('T.ind.GSPC ~ .')
-
 
 
 ###################################################
